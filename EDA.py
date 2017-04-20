fa410967c67955450c80abd7b2f4e70dee06938b
Soyoung Kim <elisoyoungkim@gmail.com>
2017-04-20 00:51:01 -0400

    Update EDA.py

--- EDA.py
+++ EDA.py
@@ -1,7 +1,6 @@
 # coding: utf-8
 
 # Rent Interest Classifier 
-
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 from subprocess import check_output
